332c7538e7fd30d3ad659cd98065bb904e477955
Charles Njenga <mwangicharles@gmail.com>
2019-08-30 11:38:40 +0300

    added dynamic content

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+
+posts = [
+    {
+        'Author': 'Charles Njenga',
+        'Title': 'Blog 1',
+        'Content': 'Blog 1 Content',
+        'Date_Posted': 'August 30, 2019'
+    },
+    {
+        'Author': 'Jane Doe',
+        'Title': 'Blog 2',
+        'Content': 'Blog 2 Content',
+        'Date_Posted': 'August 27, 20189'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
