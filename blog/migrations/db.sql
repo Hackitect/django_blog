5286dde8e1b74882f3431bd3f976881ab51ed4e1
Charles Njenga <mwangicharles@gmail.com>
2019-08-30 15:26:05 +0300

    register db models on the admin end

---
+++ blog/migrations/db.sql
@@ -0,0 +1,7 @@
+BEGIN;
+--
+-- Create model Post
+--
+CREATE TABLE "blog_post" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "title" varchar(100) NOT NULL, "content" text NOT NULL, "date_posted" datetime NOT NULL, "author_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED);
+CREATE INDEX "blog_post_author_id_dd7a8485" ON "blog_post" ("author_id");
+COMMIT;
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
 
-posts = [
-    {
-        'author': 'Charles Njenga',
-        'title': 'Blog 1',
-        'content': 'Blog 1 Content',
-        'date_posted': 'August 30, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog 2',
-        'content': 'Blog 2 Content',
-        'date_posted': 'August 27, 20189'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Charles Njenga',
+#         'title': 'Blog 1',
+#         'content': 'Blog 1 Content',
+#         'date_posted': 'August 30, 2019'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog 2',
+#         'content': 'Blog 2 Content',
+#         'date_posted': 'August 27, 20189'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
