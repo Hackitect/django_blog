067925bdd02dec8ac4f287caaca831097911a193
Charles Njenga <mwangicharles@gmail.com>
2019-08-30 12:20:28 +0300

    added bootstrap files

--- blog/views.py
+++ blog/views.py
@@ -22,8 +22,8 @@ def home(request):
     context = {
         'posts': posts
     }
-    return render(request, 'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'Wamlambez'})
