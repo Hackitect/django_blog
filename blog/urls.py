86942bbd6577a3b5b89def5fbb6a8739db15e433
Charles Njenga <mwangicharles@gmail.com>
2019-09-05 09:28:41 +0300

    added link to create new post

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
-    PostDeleteView
+    PostDeleteView,
+    PostUpdateView
 )
 from . import views
 
@@ -11,7 +12,8 @@ urlpatterns = [
     # path('', views.home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
