b92d219c2b73e3d2f7cd91f53f0baf1a8c331f74
Charles Njenga <mwangicharles@gmail.com>
2019-09-05 05:19:49 +0300

    adds post create view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView
+)
 from django.shortcuts import render
 from .models import Post
 # from django.http import HttpResponse
@@ -24,6 +30,26 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
--- users/views.py
+++ users/views.py
@@ -28,15 +28,16 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(
-                request, f'Your profile has been updated!')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
 
     else:
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = UserUpdateForm(request.POST, instance=request.user.profile)
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
